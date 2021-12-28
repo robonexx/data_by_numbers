2175ae9716579d065a5d3306d82364164e516054
robonexx <robertwagar@gmail.com>
2021-12-28 09:34:56 +0100

    new project

---
+++ index.js
@@ -0,0 +1,13 @@
+// easy programming exercise for myself
+    
+// Gathering data and counting out the average, total etc
+// First one gather male and females
+// Enter age and save the data
+// Count average age
+// buttons to add more or check result
+// use localstorage first
+
+// visual data and gathering
+//option male, female
+// age
+
